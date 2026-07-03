e295f3c49dc6d2e426fdcc05656a577398a32404
iki1uc <entertrainer@web.de>
2026-07-03 18:57:45 +0200

    Add RESPO_SPEAKWRITE class with processing logic

--- RESPO-SPEAKWRITE.js
+++ RESPO-SPEAKWRITE.js
@@ -1,2 +1,25 @@
+import { LINK } from "./link.js";
+import { PC } from "./PC.js";
+import { PQ } from "./PQ.js";
+import { RAWATOR } from "./RAWATOR.js";
+
+export class RESPO_SPEAKWRITE {
+    constructor() {
+        this.pq = new PQ();
+        this.raw = new RAWATOR();
+    }
+
+    process(pxOut) {
+        if (!pxOut) return;
+
+        const txt = PC.write(pxOut);
+        this.pq.hear(pxOut);
+
+        const move = this.raw.react(pxOut);
+
+        LINK.route("txt", txt);
+        LINK.route("move", move);
+    }
+}
 
 
