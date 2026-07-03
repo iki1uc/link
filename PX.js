9c6bf9eba3f2bf3d66ceb7f047dcb6433cd72873
iki1uc <entertrainer@web.de>
2026-07-03 18:56:53 +0200

    Update PX.js

--- PX.js
+++ PX.js
@@ -1,1 +1,25 @@
 
+export class PX {
+    constructor() {
+        this.mode = "listen";
+        this.role = null;
+        this.lastText = "";
+    }
+
+    setMode(m) {
+        this.mode = m;
+    }
+
+    setRole(r) {
+        this.role = r;
+    }
+
+    hear(text) {
+        if (this.mode !== "write") return null;
+        this.lastText = text;
+        return {
+            role: this.role,
+            text: this.lastText
+        };
+    }
+}
