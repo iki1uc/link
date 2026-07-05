f71a9bb22455b60a267052ff650ea9386c000711
iki1uc <entertrainer@web.de>
2026-07-05 07:24:36 +0200

    Update RESPO‑SPEAKWRITE.js

--- RESPO‑SPEAKWRITE.js
+++ RESPO‑SPEAKWRITE.js
@@ -1,1 +1,13 @@
 
+export class RESPO_SPEAKWRITE {
+    constructor() {
+        this.log = [];
+    }
+
+    process(pxOut) {
+        if (!pxOut) return;
+
+        this.log.push(pxOut);
+        return pxOut;
+    }
+}
